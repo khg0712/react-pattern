c17ad4945bbdfc6433617b4a2a568836849a8cc0
hugo kim <k7120792@gmail.com>
2021-02-18 01:35:23 +0900

    RequestFactory 추가

--- next/nexty/lib/RequestFactory.ts
+++ next/nexty/lib/RequestFactory.ts
@@ -1,7 +1,43 @@
 import axios from "axios";
 
-const instance = axios.create()
+const instance = axios.create({
+    baseURL: '/',
+})
 
-export default class RequestFactory {
+enum ApiRoute {}
 
+export default abstract class RequestFactory {
+    abstract createRequest(type: string): Request
+}
+
+enum MovieApiRoute {
+    list = "/"
+}
+
+export class MovieRequestFactory extends RequestFactory {
+    createRequest(type: MovieApiRoute) {
+        switch(type) {
+            case MovieApiRoute.list:
+                return new Request();
+        }
+    }
+
+}
+
+class Request {
+    constructor() {
+
+    }
+}
+
+class MovieRq extends Request {
+
+}
+
+class Model {
+    rqFactory = new MovieRequestFactory();
+
+    load() {
+        this.rqFactory.createRequest(MovieApiRoute.list)
+    }
 }
